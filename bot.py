6ac470b41c43a9536e93215e97ffd40161761e1e
z3tttt <noobikers6@gmail.com>
2024-10-18 15:54:55 +0100

    Комментарий к коммиту

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
